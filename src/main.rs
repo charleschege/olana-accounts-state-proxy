4690c41898ff127161913edac0eec547079ff980
Charles Chege <charleschege@protonmail.ch>
2022-09-12 18:08:49 +0300

    Feat: Add test to compare JSON result from Solana RPC network and Proxy server

--- src/main.rs
+++ src/main.rs
@@ -94,3 +94,49 @@ fn log() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod correctness_tests {
+    #[tokio::test]
+    async fn get_account_info() {
+        use std::process::Command;
+
+        Command::new("./target/debug/solana-accounts-proxy")
+            .arg("./tests/config_file")
+            .spawn()
+            .unwrap();
+
+        let body = json::object! {
+            "jsonrpc": "2.0",
+            "id": 1u8,
+            "method": "getAccountInfo",
+            "params": json::array![
+              "JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB",
+              json::object!{
+                "encoding": "base64",
+                "commitment": "finalized"
+              }
+            ]
+        };
+        let body = body.to_string();
+
+        std::thread::sleep(std::time::Duration::from_secs(3));
+
+        let proxy_response = minreq::post("http://127.0.0.1:4000")
+            .with_header("Content-Type", "application/json")
+            .with_body(body.clone())
+            .send()
+            .unwrap();
+
+        let rpc_response = minreq::post("https://api.mainnet-beta.solana.com")
+            .with_header("Content-Type", "application/json")
+            .with_body(body)
+            .send()
+            .unwrap();
+
+        assert_eq!(
+            proxy_response.as_str().unwrap(),
+            rpc_response.as_str().unwrap()
+        );
+    }
+}
