b65d3f54464e1bb362f1e5e52e3e32c7813b0ca6
Charles Chege <charleschege@protonmail.ch>
2022-11-28 06:24:34 +0000

    Fix: Remove unnecessary function to deserialized the IP address

--- solana-accounts-proxy/src/config.rs
+++ solana-accounts-proxy/src/config.rs
@@ -1,5 +1,5 @@
 use secrecy::Secret;
-use serde::{de, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use std::{
     env, fmt,
     fs::File,
@@ -85,7 +85,6 @@ impl ProxyConfig {
 /// Configuration specific to the IP address and port of the proxy server
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SocketConfig {
-    #[serde(deserialize_with = "ip_parser")]
     ip: Ipv4Addr,
     port: u16,
 }
@@ -154,31 +153,4 @@ impl fmt::Debug for PostgresConfig {
             .field("connect_timeout", &self.connect_timeout)
             .finish()
     }
-}
-
-fn ip_parser<'de, D>(deserializer: D) -> Result<Ipv4Addr, D::Error>
-where
-    D: de::Deserializer<'de>,
-{
-    struct StringVisitor;
-
-    impl<'de> de::Visitor<'de> for StringVisitor {
-        type Value = Ipv4Addr;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("A string containing an IP address is required")
-        }
-
-        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-        where
-            E: de::Error,
-        {
-            match v.parse() {
-                Ok(ip) => Ok(ip),
-                Err(error) => Err(serde::de::Error::custom(error.to_string())),
-            }
-        }
-    }
-
-    deserializer.deserialize_any(StringVisitor)
 }
